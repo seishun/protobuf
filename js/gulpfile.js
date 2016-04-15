71abf45ed3855071623f415e390bb034136d60a6
Nikolai Vavilov <vvnicholas@gmail.com>
2016-04-15 20:09:02 +0300

    get working on windows

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
+var fs = require('fs');
 var glob = require('glob');
+var mkdirp = require('mkdirp');
 
 var protoc = process.env.PROTOC || '../src/protoc';
 
 gulp.task('genproto_closure', function (cb) {
-  exec(protoc + ' --js_out=library=testproto_libs,binary:.  -I ../src -I . *.proto ../src/google/protobuf/descriptor.proto',
-       function (err, stdout, stderr) {
+  execFile(protoc,
+           ['--js_out=library=testproto_libs,binary:.', '-I', '../src', '-I', '.'].concat(glob.sync('*.proto'), '../src/google/protobuf/descriptor.proto'),
+           function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
@@ -14,18 +18,22 @@ gulp.task('genproto_closure', function (cb) {
 });
 
 gulp.task('genproto_commonjs', function (cb) {
-  exec('mkdir -p commonjs_out && ' + protoc + ' --js_out=import_style=commonjs,binary:commonjs_out -I ../src -I commonjs -I . *.proto commonjs/test*/*.proto ../src/google/protobuf/descriptor.proto',
-       function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+  mkdirp('commonjs_out', function (err) {
+    if (err) return cb(err);
+    execFile(protoc,
+             ['--js_out=import_style=commonjs,binary:commonjs_out', '-I', '../src', '-I', 'commonjs', '-I', '.'].concat(glob.sync('*.proto'), glob.sync('commonjs/test*/*.proto'), '../src/google/protobuf/descriptor.proto'),
+             function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
   });
 });
 
 gulp.task('dist', function (cb) {
   // TODO(haberman): minify this more aggressively.
   // Will require proper externs/exports.
-  exec('./node_modules/google-closure-library/closure/bin/calcdeps.py -i message.js -i binary/reader.js -i binary/writer.js -i commonjs/export.js -p . -p node_modules/google-closure-library/closure -o compiled --compiler_jar node_modules/google-closure-compiler/compiler.jar > google-protobuf.js',
+  exec('python ./node_modules/google-closure-library/closure/bin/calcdeps.py -i message.js -i binary/reader.js -i binary/writer.js -i commonjs/export.js -p . -p node_modules/google-closure-library/closure -o compiled --compiler_jar node_modules/google-closure-compiler/compiler.jar > google-protobuf.js',
        function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -34,57 +42,75 @@ gulp.task('dist', function (cb) {
 });
 
 gulp.task('commonjs_asserts', function (cb) {
-  exec('mkdir -p commonjs_out/test_node_modules && ./node_modules/google-closure-library/closure/bin/calcdeps.py -i commonjs/export_asserts.js -p . -p node_modules/google-closure-library/closure -o compiled --compiler_jar node_modules/google-closure-compiler/compiler.jar > commonjs_out/test_node_modules/closure_asserts_commonjs.js',
-       function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+  mkdirp('commonjs_out/test_node_modules', function (err) {
+    if (err) return cb(err);
+    exec('python ./node_modules/google-closure-library/closure/bin/calcdeps.py -i commonjs/export_asserts.js -p . -p node_modules/google-closure-library/closure -o compiled --compiler_jar node_modules/google-closure-compiler/compiler.jar > commonjs_out/test_node_modules/closure_asserts_commonjs.js',
+         function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
   });
 });
 
 gulp.task('commonjs_testdeps', function (cb) {
-  exec('mkdir -p commonjs_out/test_node_modules && ./node_modules/google-closure-library/closure/bin/calcdeps.py -i commonjs/export_testdeps.js -p . -p node_modules/google-closure-library/closure -o compiled --compiler_jar node_modules/google-closure-compiler/compiler.jar > commonjs_out/test_node_modules/testdeps_commonjs.js',
-       function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+  mkdirp('commonjs_out/test_node_modules', function (err) {
+    if (err) return cb(err);
+    exec('python ./node_modules/google-closure-library/closure/bin/calcdeps.py -i commonjs/export_testdeps.js -p . -p node_modules/google-closure-library/closure -o compiled --compiler_jar node_modules/google-closure-compiler/compiler.jar > commonjs_out/test_node_modules/testdeps_commonjs.js',
+         function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
   });
 });
 
 gulp.task('make_commonjs_out', ['dist', 'genproto_commonjs', 'commonjs_asserts', 'commonjs_testdeps'], function (cb) {
   // TODO(haberman): minify this more aggressively.
   // Will require proper externs/exports.
-  var cmd = "mkdir -p commonjs_out/binary && mkdir -p commonjs_out/test_node_modules && ";
-  function addTestFile(file) {
-    cmd += 'node commonjs/rewrite_tests_for_commonjs.js < ' + file +
-           ' > commonjs_out/' + file + '&& ';
-  }
+  mkdirp('commonjs_out/binary', function (err) {
+    if (err) return cb(err);
+    mkdirp('commonjs_out/test_node_modules', function (err) {
+      if (err) return cb(err);
+      var cmd = '';
+      function addTestFile(file) {
+        cmd += 'node commonjs/rewrite_tests_for_commonjs.js < ' + file +
+               ' > commonjs_out/' + file + '&& ';
+      }
 
-  glob.sync('*_test.js').forEach(addTestFile);
-  glob.sync('binary/*_test.js').forEach(addTestFile);
+      glob.sync('*_test.js').forEach(addTestFile);
+      glob.sync('binary/*_test.js').forEach(addTestFile);
 
-  exec(cmd +
-       'cp commonjs/jasmine.json commonjs_out/jasmine.json && ' +
-       'cp google-protobuf.js commonjs_out/test_node_modules && ' +
-       'cp commonjs/import_test.js commonjs_out/import_test.js',
-       function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+      exec(cmd +
+           'cp commonjs/jasmine.json commonjs_out/jasmine.json && ' +
+           'cp google-protobuf.js commonjs_out/test_node_modules && ' +
+           'cp commonjs/import_test.js commonjs_out/import_test.js',
+           function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+      });
+    });
   });
 });
 
 gulp.task('deps', ['genproto_closure'], function (cb) {
-  exec('./node_modules/google-closure-library/closure/bin/build/depswriter.py *.js binary/*.js > deps.js',
-       function (err, stdout, stderr) {
-    console.log(stdout);
+  execFile('python',
+           ['./node_modules/google-closure-library/closure/bin/build/depswriter.py'].concat(glob.sync('*.js'), glob.sync('binary/*.js')),
+           function (err, stdout, stderr) {
+    if (err) return cb(err);
+    fs.writeFile('deps.js', stdout, function (err) {
+      cb(err);
+    });
     console.log(stderr);
-    cb(err);
   });
 });
 
 gulp.task('test_closure', ['genproto_closure', 'deps'], function (cb) {
-  exec('JASMINE_CONFIG_PATH=jasmine.json ./node_modules/.bin/jasmine',
+  var env = Object.create(process.env);
+  env.JASMINE_CONFIG_PATH = 'jasmine.json';
+  exec('"./node_modules/.bin/jasmine"',
+       { env: env },
        function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -93,7 +119,14 @@ gulp.task('test_closure', ['genproto_closure', 'deps'], function (cb) {
 });
 
 gulp.task('test_commonjs', ['make_commonjs_out'], function (cb) {
-  exec('cd commonjs_out && JASMINE_CONFIG_PATH=jasmine.json NODE_PATH=test_node_modules ../node_modules/.bin/jasmine',
+  var env = Object.create(process.env);
+  env.JASMINE_CONFIG_PATH = 'jasmine.json';
+  env.NODE_PATH = 'test_node_modules';
+  exec('"../node_modules/.bin/jasmine"',
+       {
+         cwd: 'commonjs_out',
+         env: env
+       },
        function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
